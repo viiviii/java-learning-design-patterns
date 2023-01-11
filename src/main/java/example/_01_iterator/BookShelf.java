2b7af73bd3d7026367579ad1212e77a45ea9990c
viiviii <viiiix.viiviii@gmail.com>
2023-01-11 16:55:12 +0900

    :monocle_face: 직접 구현한 iterator를 사용하게 변경

--- src/main/java/example/_01_iterator/BookShelf.java
+++ src/main/java/example/_01_iterator/BookShelf.java
@@ -1,6 +1,5 @@
 package example._01_iterator;
 
-import java.util.Arrays;
 import java.util.Iterator;
 
 class BookShelf implements Iterable<Book> {
@@ -12,9 +11,6 @@ class BookShelf implements Iterable<Book> {
         this.books = new Book[maxLength];
     }
 
-    // Iterator next()가 하는 일
-    // - 집합체 요소를 1개 반환
-    // - 내부 상태를 다음으로 진행시킴 (숨겨진 역할)
     public void appendBook(Book book) {
         books[getLength()] = book;
         length += 1;
@@ -30,6 +26,6 @@ class BookShelf implements Iterable<Book> {
 
     @Override
     public Iterator<Book> iterator() {
-        return Arrays.stream(books).iterator();
+        return new BookShelfIterator(this);
     }
 }
