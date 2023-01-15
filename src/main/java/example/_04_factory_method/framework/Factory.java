809ba9fec8b710916aa62e04f5fba634755e5c38
viiviii <viiiix.viiviii@gmail.com>
2023-01-15 18:45:40 +0900

    :recycle: 추상 클래스 메서드 순서 조정
    - 나는 abstract method가 더 궁금할 줄 알았는데
    - 책을 보고 ChatGPT한테 물어보니 concrete method가 위에 있는게 일반적이라고 함

--- src/main/java/example/_04_factory_method/framework/Factory.java
+++ src/main/java/example/_04_factory_method/framework/Factory.java
@@ -1,14 +1,14 @@
 package example._04_factory_method.framework;
 
 public abstract class Factory {
-    protected abstract Product createProduct(String owner);
-
-    protected abstract void registerProduct(Product product);
-
     public final Product create(String owner) {
         final Product product = createProduct(owner);
         registerProduct(product);
 
         return product;
     }
+
+    protected abstract Product createProduct(String owner);
+
+    protected abstract void registerProduct(Product product);
 }
