3911575663bbda3098bfeec692a40438a367bc1e
viiviii <viiiix.viiviii@gmail.com>
2023-01-22 15:29:57 +0900

    :recycle: 연습문제 5-1: 테스트 코드 리팩토링
    - 불필요한 Order 제거
    - 동시성 테스트 inner class로 이동

--- src/test/java/example/_05_singleton/TicketMakerTest.java
+++ src/test/java/example/_05_singleton/TicketMakerTest.java
@@ -1,12 +1,10 @@
 package example._05_singleton;
 
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
@@ -15,34 +13,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestMethodOrder(value = OrderAnnotation.class)
 class TicketMakerTest {
 
+    private final TicketMaker ticketMaker = ticketMaker();
 
     @Order(1)
     @Test
-    void instanceIsSingleton() {
-        //given
-        var obj1 = ticketMaker();
-        var obj2 = ticketMaker();
-
-        //then
-        assertThat(obj1).isSameAs(obj2);
-    }
-
-    @Order(2)
-    @Test
-    void instanceIsNotNull() {
-        //given
-        var instance = ticketMaker();
-
-        //then
-        assertThat(instance).isNotNull();
-    }
-
-    @Order(3)
-    @Test
     void initialTicketNumber() {
-        //given
-        var ticketMaker = ticketMaker();
-
         //when
         var initial = ticketMaker.getNextTicketNumber();
 
@@ -50,31 +25,45 @@ class TicketMakerTest {
         assertThat(initial).isEqualTo(1000);
     }
 
-    @Order(4)
+    @Test
+    void instanceIsSingleton() {
+        //given
+        var other = ticketMaker();
+
+        //then
+        assertThat(ticketMaker).isSameAs(other);
+    }
+
     @Test
     void nextTicketNumberIncreasesOne() {
         //given
-        var ticketMaker = ticketMaker();
+        var previous = ticketMaker.getNextTicketNumber();
 
         //when
-        var next = ticketMaker.getNextTicketNumber();
+        var ticketNumber = ticketMaker.getNextTicketNumber();
 
         //then
-        assertThat(next).isEqualTo(1001);
+        assertThat(ticketNumber).isEqualTo(previous + 1);
     }
 
-    @RepeatedTest(value = 10000, name = "{currentRepetition} / {totalRepetitions}")
-    void multiThreads() throws ExecutionException, InterruptedException {
-        //given
-        var execution = Executors.newFixedThreadPool(2);
+    @Nested
+    class WhenConcurrency {
+        private final ExecutorService executor = Executors.newFixedThreadPool(2);
 
-        //when
-        Future<Integer> ticketNumber = execution.submit(() -> ticketMaker().getNextTicketNumber());
-        Future<Integer> other = execution.submit(() -> ticketMaker().getNextTicketNumber());
+        @RepeatedTest(value = 10_000, name = "{currentRepetition} / {totalRepetitions}")
+        void multiThreads() throws ExecutionException, InterruptedException {
+            //when
+            Future<Integer> ticketNumber = executor.submit(ticketMaker::getNextTicketNumber);
+            Future<Integer> other = executor.submit(ticketMaker::getNextTicketNumber);
 
-        //then
-        assertThat(ticketNumber.get()).isNotEqualTo(other.get());
-        execution.shutdownNow(); // todo: 자리가 안좋아
+            //then
+            assertThat(ticketNumber.get()).isNotEqualTo(other.get());
+        }
+
+        @AfterEach
+        void tearDown() {
+            executor.shutdownNow();
+        }
     }
 
     private TicketMaker ticketMaker() {
